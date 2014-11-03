76723c6385497fec0afdd45b1a7f566d68093d46
Ostroumov Anatolij <ostroumov095@gmail.com>
2014-11-04 01:00:08 +0300

    initial

---
+++ README.md
@@ -0,0 +1,57 @@
+REPeat FORever
+================================
+Simple console utility to repeat command given forever.
+I do not care about you, but for me this code saved my day.
+
+Usage
+================================
+`repfor [-d=3] [-o=/dev/stdout] [-e=/dev/stderr] commandToRepeatForeverWithDelayProvided`
+
+Parameters
+================================
+
+* d - delay in seconds, before repeating the command
+* o - where to stream stdout of command
+* e - where to stream stderror of command
+
+
+Examples:
+================================
+```shell
+
+	# repfor 'cat /etc/issue'
+	# repfor -d=5 -o=output.log -e=error.log 'ps -e'
+
+```
+
+Installation from source:
+================================
+We assume you have [Go](http://golang.org/) installed
+```shell
+
+	# go build -o ~/bin/repfor github.com/vodolaz095/repfor
+
+```
+
+License
+================================
+The MIT License (MIT)
+
+Copyright (c) 2013 Ostroumov Anatolij <ostroumov095 at gmail dot com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ repfor.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+func runCommand(command string, t time.Time, stdOut chan<- string, stdError chan<- string) {
+	args := strings.Split(command, " ")
+	c := args[0]
+	p := args[1:]
+	cmd := exec.Command(c, p...)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		stdError <- fmt.Sprintf("%v | %v >>>\n%v\n<<<\n", t.Format("Jan 2 15:04:05 2006"), command, err.Error())
+	}
+	stdOut <- fmt.Sprintf("%v | %v >>>\n%v\n<<<\n", t.Format("Jan 2 15:04:05 2006"), command, out.String())
+}
+
+func main() {
+	fmt.Println("+==============+")
+	fmt.Println("|REPeat FORever|")
+	fmt.Println("+==============+")
+
+	stdOut := make(chan string)
+	stdError := make(chan string)
+	var delay int64
+	var command, outlog, errorlog string
+	if len(os.Args) == 1 {
+		fmt.Println("Usage: repfor [-d=3] [-o=/dev/stdout] [-e=/dev/stderr] commandToRepeatForeverWithDelayProvided")
+		fmt.Println("Examples:\n # repfor 'cat /etc/issue' \n # repfor -d=5 -o=output.log -e=error.log 'ps -e'")
+		fmt.Println("Visit https://github.com/vodolaz095/repfor for more information")
+		os.Exit(1)
+	}
+	command = os.Args[len(os.Args)-1]
+	flag.Int64Var(&delay, "d", 3, "Interval beteen executing command")
+	flag.StringVar(&outlog, "o", "/dev/stdout", "Where to stream stdout")
+	flag.StringVar(&errorlog, "e", "/dev/stderr", "Where to stream stderr")
+
+	flag.Parse()
+	fmt.Printf("We execute `%v` with interval of  %v seconds...\n", command, delay)
+	fmt.Printf("StdOut goes to `%v`\n", outlog)
+	fmt.Printf("StdError goes to `%v`\n", errorlog)
+
+	outLogFile, err := os.OpenFile(outlog, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0660)
+	outErrorFile, err := os.OpenFile(errorlog, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0660)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		fmt.Printf("REPFOR stopped...\n")
+		outErrorFile.Close()
+		outLogFile.Close()
+	}()
+
+	timerChannel := time.Tick(time.Duration(delay) * time.Second)
+	for {
+		select {
+		case outMessage := <-stdOut:
+			outLogFile.Write([]byte(outMessage))
+		case outError := <-stdError:
+			outErrorFile.Write([]byte(outError))
+		case t := <-timerChannel:
+			go runCommand(command, t, stdOut, stdError)
+		default:
+			//do nothing
+		}
+	}
+}
